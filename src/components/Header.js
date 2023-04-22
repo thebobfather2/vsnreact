8de93147a6da5538df82cd65bee5508d4fe4de49
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-22 03:16:42 -0400

    margin on logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header ({handlePageChange, currentPage}) {
     return (
         <header style={{display: "flex", alignItems: "center", color: "#ffffff", backgroundColor: "#000000"}}>
             <h1 className='Header' style={{ textAlign: "right", textShadow: "2px, 2px, #F3E47D", color: "#ffffff" }}>
-                <img src="./img/vsnlogo.png" style={{ width: '70px', marginLeft: "40px", marginRight: "40px" }} />
+                <img src="./img/vsnlogo.png" style={{ width: '70px', marginLeft: "30px", marginRight: "30px" }} />
             </h1>
             <Navbar handlePageChange={handlePageChange} currentPage={currentPage}/>
         </header>
