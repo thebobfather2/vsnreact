daabb978b6f167dd26fd8d5ce393a254ab31a21c
Bryan Pike <bryanandrewpike@gmail.com>
2023-04-30 12:14:20 -0400

    nav style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 // import SignUpForm from './SignupForm';
 
 function AuthNavbar({ currentPage, handlePageChange }) {
     console.log(currentPage);
     return (
-        <div>
+        <div className='Navbar'>
         <nav>
-                <Link style={{ color: "#ffffff", padding: "20px", fontSize: "2.5vh" }}
+                <Link className='Links' style={{ color: "#ffffff"}}
                     to="/"
                     // This is a conditional (ternary) operator that checks to see if the current page is "Home"
                     // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
@@ -15,20 +16,20 @@ function AuthNavbar({ currentPage, handlePageChange }) {
                     Home
                 </Link>
 
-                <Link style={{ color: "#ffffff", padding: "20px", fontSize: "2.5vh" }}
+                <Link className='Links' style={{ color: "#ffffff" }}
                 to="/Marketplace"
                 // Check to see if the currentPage is `Homework`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                 >
                     Marketplace
                 </Link>
 
-                <Link style={{ color: "#ffffff", padding: "20px", fontSize: "2.5vh" }}
+                <Link className='Links'style={{ color: "#ffffff" }}
                 to="/VSIR"
                 // Check to see if the currentPage is `Homework`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                 >
                     VSIR
                 </Link>
-                <Link style={{ color: "#ffffff", padding: "20px", fontSize: "2.5vh" }}
+                <Link className='Links' style={{ color: "#ffffff" }}
                 to="/Login"
                 // Check to see if the currentPage is `Homework`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                 >
