86f2aa2fdf3d7ba0ae8eff5c31d5e4d103a4f6a8
Bryan Pike <bryanandrewpike@gmail.com>
2023-05-01 01:14:21 -0400

    mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 function AuthNavbar({ isAuthenticated, currentPage, handlePageChange }) {
-    const accessToken = localStorage.getItem('accessToken');
-    const navigate = useNavigate();
-    const [showMenu, setShowMenu] = useState(false);
+  const accessToken = localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
 
   const logout = () => {
     localStorage.removeItem('accessToken');
@@ -13,60 +21,91 @@ function AuthNavbar({ isAuthenticated, currentPage, handlePageChange }) {
   };
 
   return (
+    <div className="NavbarWrapper"> {/* add a wrapper div */}
     <div className='Navbar'>
-    <button className='MenuIcon' onClick={() => setShowMenu(!showMenu)}>
-        <i className="fas fa-bars" style={{ color: '#ffffff' }}></i>
-    </button>
-    <nav className={showMenu ? 'MobileNav' : ''}>
-      <Link
-        className='Links'
-        style={{ color: '#ffffff' }}
-        to='/'
-        onClick={() => setShowMenu(false)}
-      >
-        Home
-      </Link>
-
-      <Link
-        className='Links'
-        style={{ color: '#ffffff' }}
-        to='/Marketplace'
-        onClick={() => setShowMenu(false)}
-      >
-        Marketplace
-      </Link>
-
-      <Link
-        className='Links'
-        style={{ color: '#ffffff' }}
-        to='/VSIR'
-        onClick={() => setShowMenu(false)}
-      >
-        VSIR
-      </Link>
-      {accessToken ? (
-        <>
+      <nav>
+        <div className='Navbar__mobile'>
+          <button className='Navbar__toggle' onClick={toggleMenu}>
+            <i className='fa fa-bars' />
+          </button>
+          {showMenu && (
+           <div className='Navbar__mobile-menu'>
+           <Link className='MobileLinks' to='/' onClick={closeMenu}>
+             Home
+           </Link>
+           <Link className='MobileLinks' to='/Marketplace' onClick={closeMenu}>
+             Marketplace
+           </Link>
+           <Link className='MobileLinks' to='/VSIR' onClick={closeMenu}>
+             VSIR
+           </Link>
+           {accessToken ? (
+             <>
+               <Link className='MobileLinks' to='/Profile' onClick={closeMenu}>
+                 Profile
+               </Link>
+               <div className='logout'>
+               <button onClick={logout}>Logout</button>
+               </div>
+             </>
+           ) : (
+             <Link className='MobileLinks' to='/Login' onClick={closeMenu}>
+               Log In
+             </Link>
+           )}
+         </div>
+       )}
+     </div>
+        <div className='Navbar__desktop'>
           <Link
             className='Links'
             style={{ color: '#ffffff' }}
-            to='/Profile'
-            onClick={() => setShowMenu(false)}
+            to='/'
+            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           >
-            Profile
+            Home
           </Link>
-          
-          <button onClick={logout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <Link className='Links' style={{ color: '#ffffff' }} to='/Login' onClick={() => setShowMenu(false)}>
-          Log In
-        </Link>
-      )}
-    </nav>
-  </div>
-);
+
+          <Link
+            className='Links'
+            style={{ color: '#ffffff' }}
+            to='/Marketplace'
+            // Check to see if the currentPage is `Homework`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          >
+            Marketplace
+          </Link>
+
+          <Link
+            className='Links'
+            style={{ color: '#ffffff' }}
+            to='/VSIR'
+            // Check to see if the currentPage is `Homework`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          >
+            VSIR
+          </Link>
+          {accessToken ? (
+            <>
+              <Link
+                className='Links'
+                style={{ color: '#ffffff' }}
+                to='/Profile'
+              >
+                Profile
+              </Link>
+
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <Link className='Links' style={{ color: '#ffffff' }} to='/Login'>
+              Log In
+            </Link>
+          )}
+        </div>
+      </nav>
+    </div>
+    </div>
+  );
 }
 
 export default AuthNavbar;
