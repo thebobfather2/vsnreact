90e09918f51b6a152fbea145a5aa4d4c9bfa2cbb
Bryan Pike <bryanandrewpike@gmail.com>
2023-05-02 13:42:01 -0400

    navbar button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,7 @@ function AuthNavbar({ isAuthenticated, currentPage, handlePageChange }) {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+  const menuButtonClass = showMenu ? 'Navbar__toggle open' : 'Navbar__toggle';
 
   const closeMenu = () => {
     setShowMenu(false);
@@ -21,48 +22,48 @@ function AuthNavbar({ isAuthenticated, currentPage, handlePageChange }) {
   };
 
   return (
-    <div className="NavbarWrapper"> {/* add a wrapper div */}
+    <div className="NavbarWrapper">
     <div className='Navbar'>
       <nav>
         <div className='Navbar__mobile'>
-          <button className='Navbar__toggle' onClick={toggleMenu}>
-            <i className='fa fa-bars' />
+          <button className={menuButtonClass} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
           </button>
           {showMenu && (
-           <div className='Navbar__mobile-menu'>
-           <Link className='MobileLinks' to='/' onClick={closeMenu}>
-             Home
-           </Link>
-           <Link className='MobileLinks' to='/Marketplace' onClick={closeMenu}>
-             Marketplace
-           </Link>
-           <Link className='MobileLinks' to='/VSIR' onClick={closeMenu}>
-             VSIR
-           </Link>
-           {accessToken ? (
-             <>
-               <Link className='MobileLinks' to='/Profile' onClick={closeMenu}>
-                 Profile
-               </Link>
-               <div className='logout'>
-               <button onClick={logout}>Logout</button>
-               </div>
-             </>
-           ) : (
-             <Link className='MobileLinks' to='/Login' onClick={closeMenu}>
-               Log In
-             </Link>
-           )}
-         </div>
-       )}
-     </div>
+            <div className='Navbar__mobile-menu'>
+              <Link className='MobileLinks' to='/' onClick={closeMenu}>
+                Home
+              </Link>
+              <Link className='MobileLinks' to='/Marketplace' onClick={closeMenu}>
+                Marketplace
+              </Link>
+              <Link className='MobileLinks' to='/VSIR' onClick={closeMenu}>
+                VSIR
+              </Link>
+              {accessToken ? (
+                <>
+                  <Link className='MobileLinks' to='/Profile' onClick={closeMenu}>
+                    Profile
+                  </Link>
+                  <div className='logout'>
+                    <button onClick={logout}>Logout</button>
+                  </div>
+                </>
+              ) : (
+                <Link className='MobileLinks' to='/Login' onClick={closeMenu}>
+                  Log In
+                </Link>
+              )}
+            </div>
+          )}
+        </div>
         <div className='Navbar__desktop'>
           <Link
             className='Links'
             style={{ color: '#ffffff' }}
             to='/'
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           >
             Home
           </Link>
@@ -71,7 +72,6 @@ function AuthNavbar({ isAuthenticated, currentPage, handlePageChange }) {
             className='Links'
             style={{ color: '#ffffff' }}
             to='/Marketplace'
-            // Check to see if the currentPage is `Homework`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           >
             Marketplace
           </Link>
