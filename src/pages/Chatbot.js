840dfb0622d69909203e8e92c14dbbd716b2e730
Bryan Pike <bryanandrewpike@gmail.com>
2023-05-02 23:24:47 -0400

    fixed chatbot

--- src/pages/Chatbot.js
+++ src/pages/Chatbot.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Chatbot.css';
 
-const API_KEY = process.env.OPENAI_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Chatbot() {
     const [message, setMessage] = useState("");
