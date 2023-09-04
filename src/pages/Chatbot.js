243af634213e4ab352654bfd951943dcf6db6e1b
thebobfather2 <107475188+thebobfather2@users.noreply.github.com>
2023-09-04 07:07:19 -0400

    chatgpt upgrade

--- src/pages/Chatbot.js
+++ src/pages/Chatbot.js
@@ -22,7 +22,7 @@ function Chatbot() {
         };
         
         try {
-            const response = await fetch("https://api.openai.com/v1/completions", {
+            const response = await fetch("https://api.openai.com/v1/chat/completions", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
